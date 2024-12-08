5dd7e655f1b36b1dc613792302da12533f4a7828
KmiloZ <3107320340407@ingenieria.usac.edu.gt>
2024-12-07 19:02:37 -0600

    [ADD]: Endpoints Registro e Inicio de Sesión
    Se agregan los endpoints y funcionalidad de registro e inicio de sesión

--- Practica/Backend/index.js
+++ Practica/Backend/index.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
 app.use(cors({origin: '*'}));
 
 // Rutas
 app.use("/api/client", require("./routes/client.routes"));
 app.use("/api/personal", require("./routes/personal.routes"))
+app.use("/api/user", require("./routes/user.routes"))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
